4ad1588fb173db92d53edaa3d2c4e9f456f62eb9
Robert Beeger <robert@beeger.net>
2011-02-25 16:54:52 +0100

    UI gebastelt

--- app/views/neu.js
+++ app/views/neu.js
@@ -37,14 +37,39 @@ zeMobile.neu = M.PageView.design({
 
     content: M.ScrollView.design({
 
-        childViews: 'labelZeit labelProject label',
+        childViews: 'neuForm',
 
-        label: M.LabelView.design({
+        neuForm: M.FormView.design({
+            childViews: 'textZeitVon textZeitBis textProject textTaetigkeit buttonEintragen',
 
-            value: 'neu3'
+            textZeitVon: M.TextFieldView.design({
+                label: 'Zeit',
+                name: 'zeitVon',
+                inline: YES
+            }),
 
+            textZeitBis: M.TextFieldView.design({
+                name: 'zeitBis',
+                inline: YES
+            }),
+
+            textProject: M.TextFieldView.design({
+                label: 'Project',
+                name: 'project'
+            }),
+
+            textTaetigkeit: M.TextFieldView.design({
+                label: 'Tätigkeit',
+                name: 'taetigkeit'
+            }),
+
+            buttonEintragen: M.ButtonView.design({
+                value: 'Eintragen',
+                type: 'submit',
+                target: zeMobile.EintragenController,
+                action: 'neueZeitEintragen'
+            })
         })
-
     }),
 
     tabs: zeMobile.tabsView
